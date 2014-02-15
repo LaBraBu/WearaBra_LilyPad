d9f92187ec5c99c958322ec7c31087d8b6590f85
hirose504 <hirose504@yahoo.co.jp>
2014-02-15 23:30:02 +0900

    プルアップ抵抗を有効に

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,12 +1,13 @@
 void setup()
 {
   Serial.begin(9600);
-  pinMode(2,OUTPUT);  // ローインピーダンス
+  pinMode(2,INPUT);  // ローインピーダンス
+  digitalWrite(2,HIGH);
 }
 
 void loop()
 {
-  if (digitalRead(2) == LOW)
+  if (digitalRead(2) == HIGH)
   {
     Serial.println("off");
     digitalWrite(13, LOW);  // 消灯
