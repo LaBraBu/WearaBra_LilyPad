8da359541ab09477ff2cbbd9c98f5254be84c581
coquerico <coquerico.saka@gmail.com>
2014-02-15 22:46:56 +0900

    add ドラクエ呪いの音楽

--- buzzerDQ/buzzerDQ.ino
+++ buzzerDQ/buzzerDQ.ino
@@ -7,6 +7,15 @@ const int G   = 3156; // ソ
 const int F   = 2794; // ファ
 const int Ef  = 2489; // ミb
 
+const int C7  = 2093; // ド
+const int B6  = 1976; // シ
+const int Bf6 = 1865; // シb
+const int A6  = 1661; // ラ
+const int G6  = 1568; // ソ
+const int E6  = 1319; // ミ
+const int B5  =  988; // シ
+const int Bf5 =  932; // シb
+
 void setup() {
     pinMode(buzpin, OUTPUT);
     Serial.begin(9600);
@@ -44,6 +53,34 @@ void buzSuccess(){
     beep(buzpin,F,300);
     delay(400);
 }
+
+void soundNoroi(){
+    beep(buzpin,B5,200);
+    beep(buzpin,B6,200);
+    beep(buzpin,B5f,200);
+    beep(buzpin,B6f,200);
+
+    beep(buzpin,B5,200);
+    beep(buzpin,B6,200);
+    beep(buzpin,B5f,200);
+    beep(buzpin,B6f,200);
+
+    beep(buzpin,B5,200);
+    beep(buzpin,B6,200);
+    beep(buzpin,B5f,200);
+    beep(buzpin,B6f,200);
+
+    beep(buzpin,B5,200);
+    beep(buzpin,B6,200);
+    beep(buzpin,B5f,200);
+    beep(buzpin,B6f,200);
+
+    beep(buzpin,E6,500);
+    beep(buzpin,G6,100);
+    beep(buzpin,Af6,100);
+    beep(buzpin,Bf5,100);
+}
+
 void loop(){
     buzSuccess();
 }
