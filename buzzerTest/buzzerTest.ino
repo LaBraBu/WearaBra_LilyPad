fdf1887694f4333d51b6b0bad3524f9ed67b76b4
putan <hiranono@gmail.com>
2014-02-15 18:38:41 +0900

    音追加、コメント修正

--- buzzerTest/buzzerTest.ino
+++ buzzerTest/buzzerTest.ino
@@ -3,6 +3,9 @@ const int Cfre   = 2093; // ドの周波数
 const int Dfre   = 2349; // レの周波数
 const int Efre   = 2637; // ミの周波数
 const int Ffre   = 2794; // ファの周波数
+const int Gfre   = 3136; // ソ
+const int Afre   = 3520; // ラ
+const int Bfre   = 3951; // シ
 
 void setup() {
     pinMode(buzpin, OUTPUT);
@@ -11,12 +14,12 @@ void setup() {
 /**
  * @param int  buzpin ポート番号
  * @param int  fre    音の周波数
- * @param long timel1 msec
+ * @param long timel1 ならす時間[msec]
  */
-void beep (int buzpin, int fre, long timel1){ // the sound producing 
+void beep (int buzpin, int fre, long time){ // the sound producing 
     int x; 
     long delayHL = (long)(1000000/fre/2); 
-    long loopn   = (long)((timel1*1000)/(2*delayHL));
+    long loopn   = (long)((time*1000)/(2*delayHL));
     for (x=0;x<loopn;x++) {
         digitalWrite(buzpin,HIGH); 
         delayMicroseconds(delayHL); 
